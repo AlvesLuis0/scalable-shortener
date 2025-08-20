4ef96320f8ec5422445b5bf54cb87e50c3b22495
AlvesLuis0 <alves.luis.3002@gmail.com>
2025-08-20 18:33:40 -0300

    feat: add redirect route

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -4,10 +4,15 @@ class UrlsController < ApplicationController
     id = (Url.maximum(:id) || 0) + 1
     @url.short_code = GenerateShortCodeService.new(id).call
     if @url.save
-      render json: @url.to_json(except: [ :id, :original_url ]), status: :created, location: @url
+      render json: @url.to_json(except: [ :id, :original_url ], methods: :shorted_url), status: :created, location: @url
     else
       render json: @url.errors, status: :unprocessable_content
     end
+  end
+
+  def redirect_to_original_url
+    @url = Url.find_by!(short_code: params.expect!(:short_code))
+    redirect_to @url.original_url, allow_other_host: true
   end
 
   private
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,9 +1,15 @@
 class Url < ApplicationRecord
+  include Rails.application.routes.url_helpers
+
   MAXIMUM_ORIGINAL_LENGTH = 1024
   MAXIMUM_SHORT_CODE_LENGTH = 11
 
   with_options presence: true do |u|
     u.validates :original_url, length: { maximum: MAXIMUM_ORIGINAL_LENGTH }
     u.validates :short_code, length: { maximum: MAXIMUM_SHORT_CODE_LENGTH }, uniqueness: true
+  end
+
+  def shorted_url
+    short_redirect_url(short_code: short_code)
   end
 end
