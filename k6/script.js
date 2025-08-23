3ea4f4bad5b013881d14a62b30e9a3053d0fc800
AlvesLuis0 <alves.luis.3002@gmail.com>
2025-08-23 15:29:28 -0300

    test: add more stress tests

--- k6/script.js
+++ k6/script.js
@@ -1,22 +1,23 @@
 import http from 'k6/http';
-import {check, sleep} from 'k6';
+import { check, sleep } from 'k6';
 
 const BASE_URL = 'http://localhost:3000';
 
 export const options = {
   stages: [
-    { duration: '30s', target: 50 },
-    { duration: '1m', target: 50 },
-    { duration: '30s', target: 0 },
-  ]
+    { duration: '10s', target: 2 },
+    { duration: '15s', target: 5 },
+    { duration: '3m', target: 600 },
+  ],
 };
 
-export default function() {
+export default function () {
   // must be success
   const createRes = http.post(
-    `${BASE_URL}/urls`, JSON.stringify({original_url: "https://quickpizza.grafana.com/" }),
-    { headers: { 'Content-Type': 'application/json' },
-  });
+    `${BASE_URL}/urls`,
+    JSON.stringify({ original_url: 'https://quickpizza.grafana.com/' }),
+    { headers: { 'Content-Type': 'application/json' } }
+  );
 
   check(createRes, {
     'status is 201': (r) => r.status === 201,
@@ -25,14 +26,28 @@ export default function() {
 
   if (createRes.status === 201) {
     const shorted_url = createRes.json('shorted_url').toString();
-    const getRes = http.get(shorted_url, { redirects: 0 });
+
+    // Request com tag fixa
+    const getRes = http.get(shorted_url, {
+      redirects: 0,
+      tags: { name: 'shorted_redirect' },
+    });
     check(getRes, { 'GET status is 302': (r) => r.status === 302 });
-    const getResCache = http.get(shorted_url, { redirects: 0 });
+
+    // Cache test também com tag fixa
+    const getResCache = http.get(shorted_url, {
+      redirects: 0,
+      tags: { name: 'shorted_redirect_cache' },
+    });
     check(getResCache, { 'GET status is 302': (r) => r.status === 302 });
   }
-  // must be fail
-  const getRes = http.get(`${BASE_URL}/1`, { redirects: 0 });
-  check(getRes, { 'GET status is 404': (r) => r.status === 404 })
+
+  // must be fail - já é fixo, não explode cardinalidade
+  const getRes = http.get(`${BASE_URL}/1`, {
+    redirects: 0,
+    tags: { name: 'not_found_url' },
+  });
+  check(getRes, { 'GET status is 404': (r) => r.status === 404 });
 
   sleep(1);
 }
